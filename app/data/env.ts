00d0fd787b52b016f7b7d78373562843bbec96ae
Nick Saunders <nick@saunde.rs>
2024-07-24 09:43:10 -0700

    Add VITE_ prefix for envvars needed at build time.

--- app/data/env.ts
+++ app/data/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const schema = z.object({
-  GITHUB_ACCESS_TOKEN: z.string().min(1),
+  VITE_GITHUB_ACCESS_TOKEN: z.string().min(1),
 });
 
 export type Env = z.infer<typeof schema>;
