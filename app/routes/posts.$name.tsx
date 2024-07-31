1033e0654193c5343d43b3607933a57a3776386f
Nick Saunders <nick@saunde.rs>
2024-07-31 07:51:10 -0700

    Post stats

--- app/routes/posts.$name.tsx
+++ app/routes/posts.$name.tsx
@@ -1,13 +1,16 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import type { ReactNode } from "react";
 import { renderToString } from "react-dom/server";
 import Markdown from "react-markdown";
+import readingTime from "reading-time";
 import { createHighlighter } from "shiki";
 
 import * as Posts from "~/data/posts.js";
 import { Block } from "~/reusable/block.js";
 import { Box } from "~/reusable/box.js";
+import { ClientOnly } from "~/reusable/client-only.js";
 import { blue, gray, white } from "~/reusable/colors.js";
 import { monospace } from "~/reusable/fonts.js";
 import { TextLink } from "~/reusable/text-link.js";
@@ -154,6 +157,14 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return res;
 }
 
+function LabelValuePair({ children }: { children?: ReactNode }) {
+  return (
+    <Box display="flex" gap={4} alignItems="center">
+      {children}
+    </Box>
+  );
+}
+
 export default function Post() {
   const post = useLoaderData<typeof loader>();
   return (
@@ -176,6 +187,53 @@ export default function Post() {
             </Box>
             <Box is="p" fontSize={24}>
               {post.description}
+            </Box>
+            <Box
+              display="flex"
+              gap={16}
+              alignItems="center"
+              color={gray[70]}
+              dark:color={gray[30]}>
+              <LabelValuePair>
+                <svg
+                  style={{ width: "1em", height: "1em" }}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
+                </svg>
+                <span>
+                  <ClientOnly>
+                    {new Date(post.published).toLocaleDateString(undefined, {
+                      dateStyle: "long",
+                    })}
+                  </ClientOnly>
+                </span>
+              </LabelValuePair>
+              <LabelValuePair>
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-clock">
+                  <circle cx="12" cy="12" r="10" />
+                  <polyline points="12 6 12 12 16 14" />
+                </svg>
+                <span>
+                  {readingTime(post.markdown).minutes.toFixed(0)} minutes
+                </span>
+              </LabelValuePair>
             </Box>
           </Box>
         </Block>
